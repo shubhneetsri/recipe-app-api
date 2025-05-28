16572b85c14477ab43574e3a33eb5dced4193031
shubhneet.srivastava <shubhneet.srivastava@magicedtech.com>
2025-05-28 11:32:24 +0530

    Configure docker compose and checks to wait for db

--- app/app/settings.py
+++ app/app/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'HOST': os.environ.get('DB_HOST'),
-        'NAME': os.environ.get('DB_USER'),
+        'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASS'),
         # 'ENGINE': 'django.db.backends.sqlite3',
